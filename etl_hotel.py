5584beedd7dbabcaf9c6ee2810290af100082400
Ishita <ishitajaisia@gmail.com>
2021-05-01 01:11:08 -0400

    Update etl_hotel.py

--- etl_hotel.py
+++ etl_hotel.py
@@ -50,22 +50,6 @@ class DataGenerator:
 
 myDataGen = DataGenerator(10000)
 myDataFrame = myDataGen.genDataset()
-#myDataFrame.insert(loc = 7, column = 'NumofRooms', value = 0)
-
-#pr = [0.63,0.37]
-
-#for i in range(10000):
-#	x = myDataFrame.loc[i, 'NumOfPeople']
-#	if(x%2==0):
-#		y=x/2
-#		levels = [y,x]
-#	else:
-#		y=(x/2)+1
-#		levels = [y,x]
-
-#	myDataFrame.loc[i, 'NumofRooms'] = np.random.choice(levels, size = 1, p = pr).astype(int)
-
-
 
 myDataFrame.to_csv('etl_hotel.csv', index = False)
 myDataFrame.to_excel('etl_hotel.xlsx', index = False)
