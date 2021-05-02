47ee013c6dcabdbf7297aa283d90a12b1afe0d61
Ishita-96 <ishitajaisia@gmail.com>
2021-05-01 21:09:31 -0400

    UserNames generated

---
+++ user_name.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+from pydbgen import pydbgen
+import matplotlib.pyplot as plt
+
+class UserNameGenerator:
+    def __init__(self, datasetSize = 1):
+        self.pyDb = pydbgen.pydb()
+        self.datasetSize = datasetSize
+
+    def getUserName(self, size):
+        return (np.array([self.pyDb.fake.email() for i in range(size)]))
+
+    def genDataset(self):
+        size = self.datasetSize
+        data = {
+            'UserName' : self.getUserName(size)
+        }
+        return (pd.DataFrame(data))
+
+usernameGen = UserNameGenerator(1000000)
+usernameDF = usernameGen.genDataset()
+usernameDF.to_csv('username.csv', index = False)
