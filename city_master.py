24507aaaa73fb0aef5050f96cf637ac852df0992
amateurPotato <ruchikau8@gmail.com>
2021-05-03 19:28:48 -0400

    cityid update

--- city_master.py
+++ city_master.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from pydbgen import pydbgen
 import matplotlib.pyplot as plt
+import uuid
 
 class DataGenerator:
     def __init__(self, datasetSize = 1):
@@ -16,12 +17,16 @@ class DataGenerator:
         citytypearr = np.random.choice(levels, size = size, p = prob)
         return citytypearr
 
+    def getCityID(self, size):
+        return [str(uuid.uuid4()) for _ in range(size)]
+
     # Generate complete dataset with properties containing array of described values
     def genDataset(self):
         size = self.datasetSize
         data = {
             'CityName' : self.getCityName(size),
-            'CityType' : self.getCityType(size)
+            'CityType' : self.getCityType(size),
+            'CityId'   : self.getCityID(size)
         }
         return (pd.DataFrame(data))
 
