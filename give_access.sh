920cd60d611886919c6c77fa4e0018e1b9ecba3c
Aanchal Khandelwal <ak8257@hlog-1.hpc.nyu.edu>
2021-05-03 20:19:47 -0400

    fixes

---
+++ give_access.sh
@@ -0,0 +1,5 @@
+hdfs dfs -setfacl -R -m user:ij2056:rwx /user/ak8257/Pegasus/
+hdfs dfs -setfacl -m user:ij2056:--x /user/ak8257
+
+hdfs dfs -setfacl -R -m user:ru2025:rwx /user/ak8257/Pegasus/
+hdfs dfs -setfacl -m user:ru2025:--x /user/ak8257
