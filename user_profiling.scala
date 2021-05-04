104d626f736f64c0246b8cc3067be35e4318c339
Ruchika Upadhyay <ruchikau8@gmail.com>
2021-05-04 00:00:13 -0400

    updated path

--- user_profiling.scala
+++ user_profiling.scala
@@ -1,10 +1,10 @@
-val bounced_files:String = "Pegasus/bounced_data/"
+val bounced_files:String = "/user/ak8257/Pegasus/bounced_data/"
 val bounced_rdd = sc.textFile(bounced_files)
 
-val transaction_files:String = "Pegasus/transaction_data/"
+val transaction_files:String = "/user/ak8257/Pegasus/transaction_data/"
 val transaction_rdd = sc.textFile(transaction_files)
 
-val users_file:String = "Pegasus/other_data/user_master.csv"
+val users_file:String = "/user/ak8257/Pegasus/other_data/user_master.csv"
 val users_rdd = sc.textFile(users_file)
 
 val header = bounced_rdd.first()
@@ -58,5 +58,5 @@ df_j = df_j.withColumn("inactivity_ratio", col("dfftd.datediff") / col("adgbt"))
 
 df_j = df_j.withColumn("loyalty_bucket", when($"dftxn.num_txn" >= 5, "Gold").otherwise(when($"dftxn.num_txn" > 2 && $"dftxn.num_txn" < 5, "Silver").otherwise(when($"dftxn.num_txn" < 2 && $"dftxn.num_txn" > 0, "Bronze").otherwise("New"))))
 
-df_j.write.mode("overwrite").csv("Pegasus/user_profiling/")
+df_j.write.mode("overwrite").csv("/user/ak8257/Pegasus/user_profiling/")
 
